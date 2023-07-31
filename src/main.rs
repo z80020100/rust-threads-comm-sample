bbdda6b498840fe08a856982d4c903b05912b2da
Cliff Wu <z800201002005@gmail.com>
2023-07-31 16:49:07 +0900

    Add a Rust sample to demo thread channel communication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::sync::mpsc::channel;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let thread = thread::current();
+    println!("{:?}: main thread start", thread.id());
+    let (tx, rx) = channel();
+
+    let sender = thread::spawn(move || {
+        let thread = thread::current();
+        let data = "Hello, thread!";
+        println!("{:?}: send \"{}\"", thread.id(), data);
+        tx.send(data).expect("Unable to send on channel");
+    });
+
+    let receiver = thread::spawn(move || {
+        let thread = thread::current();
+        let data = rx.recv().expect("Unable to receive from channel");
+        println!("{:?}: reveive \"{}\"", thread.id(), data);
+    });
+
+    sender.join().expect("The sender thread has panicked");
+    receiver.join().expect("The receiver thread has panicked");
+    println!("{:?}: main thread end", thread.id());
 }
